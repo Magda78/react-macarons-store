7cd7b0b8aed5c5ca9f908c9f2be922a053eaf2b7
Magda <magda.78@hotmail.com>
2020-10-17 22:48:16 -0400

    json file

--- src/components/Products/products.js
+++ src/components/Products/products.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React ,{useState, useEffect} from 'react';
 import './products.css';
 import Item from '../Item/item';
+import items from '../../data/products.json'
 
 function Products() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        setProducts(items);
+      },[]);
+
+      console.log(products)
     return (
         <div className='products'>
             <h3>Our Products</h3>
             <div className='product__list'>
-                <Item />
+                {products.map(product => (
+                    <Item />
+                ))}
             </div>
         </div>
     )
