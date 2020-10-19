aaf877a9ad7fa03463369131cc0742d5508d0304
Magda <magda.78@hotmail.com>
2020-10-18 22:26:57 -0400

    setting up redux

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './navbar.css';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { connect } from 'react-redux';
+import toogleCartHidden from '.././redux/cart/cart.action'
 
-function Navbar() {
+function Navbar({ toogleCartHidden }) {
 	return (
 		<div className="navbar">
 			<div className="navbar__items">
 				<h3>Products</h3>
 				<h3>About</h3>
-				<ShoppingCartIcon />
+				<ShoppingCartIcon onClick={toogleCartHidden}/>
 			</div>
 		</div>
 	);
 }
 
-export default Navbar;
+const mapDispatchToProps = dispatch => ({
+	toogleCartHidden: () => dispatch(toogleCartHidden())
+});
+
+export default connect(null, mapDispatchToProps)(Navbar);
