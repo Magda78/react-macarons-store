ac06c4bb3274df3ad87bcabbf8df692376533974
Magda <magda.78@hotmail.com>
2020-10-29 23:09:29 -0400

    adding firebase

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -5,13 +5,14 @@ import { connect } from 'react-redux';
 import { toogleCartHidden } from '../redux/cart/cart.action';
 import { Link } from 'react-router-dom';
 import Cart from '../Cart/cart';
+import { signInWihGoogle } from '../../firebase'
 
 function Navbar({ toogleCartHidden, hidden }) {
 	return (
 		<div className="navbar">
 			<div className="navbar__items">
 				<Link to='/'><h3>Shop</h3></Link>
-				<h3>SignIn</h3>
+				<h3 onClick={signInWihGoogle}>SignIn</h3>
 				<ShoppingCartIcon onClick={toogleCartHidden}/>
 			</div>
 			{hidden ? null : <Cart/>}
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_kK-D92_K0DjP3Rw8civDbiErxxR2M_U",
+    authDomain: "macarons-store.firebaseapp.com",
+    databaseURL: "https://macarons-store.firebaseio.com",
+    projectId: "macarons-store",
+    storageBucket: "macarons-store.appspot.com",
+    messagingSenderId: "585795000944",
+    appId: "1:585795000944:web:08ce3bb7fbeb62f1a9f2b5",
+    measurementId: "G-7VKZL51EJX"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  export const signInWithGoogle = () => signInWithPopup(provider)
+
+  export { auth, provider };
+  //export default db;
+  export default firebase;
